261564236eba56b217bca6941160f459be3265cc
crystalDf <chendong333@gmail.com>
2015-09-05 16:02:46 +0800

    Chapter 07 Workout finished

--- app/src/main/java/com/star/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/star/workout/WorkoutDetailFragment.java
@@ -1,21 +1,49 @@
 package com.star.workout;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class WorkoutDetailFragment extends Fragment {
+
+    public static final String WORKOUT_ID = "workoutId";
 
     private long workoutId;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        if (savedInstanceState != null) {
+            workoutId = savedInstanceState.getLong(WORKOUT_ID);
+        }
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        View view = getView();
+
+        if (view != null) {
+            Workout workout = Workout.workouts[((int) workoutId)];
+
+            TextView title = (TextView) view.findViewById(R.id.text_title);
+            title.setText(workout.getName());
+
+            TextView description = (TextView) view.findViewById(R.id.text_description);
+            description.setText(workout.getDescription());
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putLong(WORKOUT_ID, workoutId);
+        super.onSaveInstanceState(outState);
     }
 
     public void setWorkoutId(long workoutId) {
